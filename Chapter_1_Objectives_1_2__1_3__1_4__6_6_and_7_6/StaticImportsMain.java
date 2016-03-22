9b444391d46168bd7bde32bcc2b8a0e5c98e21a2
Andrei Puf <pufandreipetru@gmail.com>
2016-03-22 15:42:54 +0200

    Chapter 1 Objective 1.2, 1.3, 1.4, 6.6 and 7.6

---
+++ Chapter_1_Objectives_1_2__1_3__1_4__6_6_and_7_6/StaticImportsMain.java
@@ -0,0 +1,12 @@
+import static java.lang.System.out;
+/*
+	Compile the file using the command 'javac StaticImportMina.java'
+	Run the binary file using the command 'java StaticImportMain'  
+	RESULTS:
+	Static Import works
+*/
+public class StaticImportsMain {
+	public static void main(String[] args) {
+		out.println("Static import works.");		
+	}
+}
