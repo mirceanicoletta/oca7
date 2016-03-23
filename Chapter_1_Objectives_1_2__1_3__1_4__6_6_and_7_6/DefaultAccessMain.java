bd84267831da6125eda0033ca8978e5f79790d12
Andrei Puf <pufandreipetru@gmail.com>
2016-03-23 21:39:27 +0200

    Chapter 1 Objectives 1.2, 1.3, 1.4, 6.6 and 7.6

--- Chapter_1_Objectives_1_2__1_3__1_4__6_6_and_7_6/DefaultAccessMain.java
+++ Chapter_1_Objectives_1_2__1_3__1_4__6_6_and_7_6/DefaultAccessMain.java
@@ -1,6 +1,14 @@
+/*
+	Attemplt to complyle the file using the command 'javac DefaultAccessMain.java'
+	RESULTS
+	DefaultAccessMain.java:1: error: Beverage is not public in cert; cannot be accessed from outside package
+	import cert.Beverage;
+                    ^
+	1 error
+*/
 import cert.Beverage;
 public class DefaultAccessMain {
 	public static void main (String[] args) {
-		
+		//Do something with the drink		
 	}
 }
